263cec5674358a80d4aa0e99ec2481bec650d787
PUNEETH REDDY PODDUTOORI <ppuneethreddy20@gmail.com>
2018-05-17 15:51:59 -0700

    Update main.go

--- main.go
+++ main.go
@@ -41,6 +41,15 @@ func MethodologyHandler(w http.ResponseWriter, r *http.Request){
 //	generateHTML(w,nil,"index","poster")
 
 //}
+// Get the Port from the environment so we can run on Heroku
+func GetPort() string {
+ 	var port = os.Getenv("PORT")
+ 	// Set a default port if there is nothing in the environment
+ 	if port == "" {
+ 		port = "4747"
+ 		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+ 	}	return ":" + port
+ }
 
 func main() {
 	http.HandleFunc("/", IntroductionHandler)
@@ -51,6 +60,6 @@ func main() {
 	fs := http.FileServer(http.Dir(templatesdirectoryPath))
 	http.Handle(imagesPath, fs)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(GetPort(), nil))
 }
 
